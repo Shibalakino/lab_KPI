b53c646d0da0726b4c51eb539661d0b875812280
Oleg Boyko <57860598+Shibalakino@users.noreply.github.com>
2020-10-13 13:07:49 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+void sum(float a, float d, float n) {
+	std::cout << n * (2 * a + (n - 1) * d) / 2;
+
+}
+int main() {
+	float a, d, n;
+	std::cout << "Enter values of 'a', 'd', and 'n' for sum of arithmetic progression through space \n" ;
+	std::cin >> a >> d >> n;
+	sum(a, d, n);
+	return 0;
+}
