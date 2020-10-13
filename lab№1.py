cd965c976c7f597b508230b4ca538df3a695b492
Oleg Boyko <57860598+Shibalakino@users.noreply.github.com>
2020-10-13 13:07:08 +0300

    Add files via upload

---
+++ lab№1.py
@@ -0,0 +1,7 @@
+import math
+a = float(input("Enter value of 'a' \n"))
+d = float(input("Enter value of 'd' \n"))
+n = float(input("Enter value of 'n' \n"))
+s = (((2*a)+(d*(n-1)))/2)*n
+print("your sum :", s)
+
