47eef95970505c8430c401163b429ed5402f9545
Oleg Boyko <57860598+Shibalakino@users.noreply.github.com>
2020-10-18 16:30:49 +0300

    Update lab№1.py

--- lab№1.py
+++ lab№1.py
@@ -1,7 +1,7 @@
 import math
+print("Бойко Олег ІС-01");
 a = float(input("Enter value of 'a' \n"))
 d = float(input("Enter value of 'd' \n"))
 n = float(input("Enter value of 'n' \n"))
-s = (((2*a)+(d*(n-1)))/2)*n
+s = (((2 * a) + (d * (n - 1))) / 2) * n
 print("your sum :", s)
-
